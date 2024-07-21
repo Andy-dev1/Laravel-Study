e936608a0a5b60e77da13e720733a37b51017dff
Andy <andre.andre.jazz@gmail.com>
2024-07-21 12:31:20 -0300

    first commit

---
+++ tracker/app/Http/Controllers/PizzaController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Pizza;
+use Inertia\Inertia;
+
+class PizzaController extends Controller
+{
+    public function index(){
+        $pizzas= Pizza::all();
+        return Inertia::render('Pizzas/All',['pizzas'=>$pizzas]);
+    }
+}
