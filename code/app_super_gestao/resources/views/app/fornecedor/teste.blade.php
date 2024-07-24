9b64652cfee7c07e72169e30ca6b97aa6e46cbd9
Andy <andre.andre.jazz@gmail.com>
2024-07-24 11:24:08 -0300

    blade study controller parameters

---
+++ code/app_super_gestao/resources/views/app/fornecedor/teste.blade.php
@@ -0,0 +1,14 @@
+<h3>Fornecedor</h3>
+
+{{'Texto de teste'}}
+<br>
+<?= 'Texto de teste' ?>
+<br>
+{{-- Fica o comentario que será descartado pelo interpretador do blade --}}
+
+@php
+    //Para comentarios de uma linha
+    /* Par comentarios de ultima linha */
+
+    echo 'Texto de teste';
+@endphp
--- code/app_super_gestao/routes/web.php
+++ code/app_super_gestao/routes/web.php
@@ -1,28 +1,66 @@
 <?php
 
 use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\FornecedorController;
 use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\SobreNosController;
+use App\Http\Controllers\TesteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PrincipalController::class, 'principal']);
-Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos']);
-Route::get('/contato', [ContatoController::class, 'contato']);
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
+Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/login', function () {
+    return 'Login';
+})->name('site.login');
+
+
+//Agrupando rotas para /aapp
+Route::prefix('/app')->group(function () {
+    Route::get('/clientes', function () {
+        return 'clientes';
+    })->name('app.clientes');
+    Route::get('/fornecedores', [FornecedorController::class,'index'])->name('app.fornecedores');
+    Route::get('/produtos', function () {
+        return 'produtos';
+    })->name('app.produtos');
+});
+//Redirecionamento de rota
+// Route::get('/rota1', function () {
+//     echo 'Rota 1';
+// })->name('site.rota1');
+
+
+// Route::get('/rota2', function () {
+//     return redirect()->route('site.rota1');
+// })->name('site.rota2');
+
+//Route::redirect('/rota2','/rota1');
+
+//Encaminhando parametros da rota para o controlador
+Route::get('/teste/{p1}/{p2}', [TesteController::class, 'teste'])->name('teste');
+//Rota de fallback ou rota de contingência
+
+Route::fallback(function () {
+    echo "A rota acessada não existe. <a href=" . route('site.index') . ">Clique aqui</a> para ir para página inicial";
+});
+
+
 //Opcionais da direita para esquerda
 // Route::get('/contato/{nome}/{categoria}/{assunto}/{mensagem?}', function (string $nome, string $categoria, string $assunto, string $mensagem = 'mensagem não informada') {
 //     echo "Estamos aqui:" . $nome . "-" . $categoria . "-" . $assunto . "-" . $mensagem;
 // });
 
-Route::get(
-    '/contato/{nome}/{categoria_id?}',
-    function (
-        string $nome,
-        int $categoria_id = 1 //1 - "Informação",
+// Route::get(
+//     '/contato/{nome}/{categoria_id?}',
+//     function (
+//         string $nome,
+//         int $categoria_id = 1 //1 - "Informação",
 
-    ) {
-        echo "Estamos aqui:" . $nome . "-" . $categoria_id;
-    }
-)->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
+//     ) {
+//         echo "Estamos aqui:" . $nome . "-" . $categoria_id;
+//     }
+// )->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
 
 // Route::get('/', function () {
 //     return 'Hello World';
