8d701d3547330effc94474e8c741be807390583f
Andy <andre.andre.jazz@gmail.com>
2024-07-29 15:15:09 -0300

    Eloquent queries

--- code/app_super_gestao/app/Models/Fornecedor.php
+++ code/app_super_gestao/app/Models/Fornecedor.php
@@ -7,5 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fornecedor extends Model
 {
+    protected $table = "fornecedores";//$f->save();
+    protected $fillable=['nome','site','uf','email'];
+    //\App\Models\Fornecedor::create(['nome'=>"Fornecedor ABC",'site'=>'fornecedorabc.com.br','uf'=>'SP','email'=>'contato@fornecedorabc.com.br']);
+    //php artisan tinker
+    //     use \App\Models\Fornecedor;
+    // > $fornecedores= Fornecedor::all();
+    // $fornecedores2 = Fornecedor::find(2)
+    // $fornecedores2 = Fornecedor::find([1,2])
     use HasFactory;
 }
--- code/app_super_gestao/app/Models/SiteContato.php
+++ code/app_super_gestao/app/Models/SiteContato.php
@@ -7,5 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class SiteContato extends Model
 {
+
+    /*
+    php artisan tinker
+    use \App\Models\SiteContato;
+
+    $contatos=SiteContato::where('id','>',1)->get();
+    $contatos=SiteContato::where('motivo_contato','<=',2)->get();
+    $contatos=SiteContato::where('nome','<>','Maria')->get();
+    $contatos=SiteContato::where('nome','Maria')->get(); ==
+    $contatos=SiteContato::where('mensagem','like','%detalhes%')->get();
+
+    $contatos = SiteContato::whereIn('motivo_contato',[1,3])->get();
+    $contatos = SiteContato::whereNotIn('motivo_contato',[1,3])->get();
+    $contatos = SiteContato::whereBetween('id',[3,6])->get();
+    $contatos = SiteContato::whereNotBetween('id',[3,6])->get();
+    */
     use HasFactory;
 }
