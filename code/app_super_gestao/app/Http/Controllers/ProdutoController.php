0fd6eb483340224f04e61a8bd28484943e5bdd07
Andy <andre.andre.jazz@gmail.com>
2024-08-14 16:59:39 -0300

    associacao produto fornecedor

--- code/app_super_gestao/app/Http/Controllers/ProdutoController.php
+++ code/app_super_gestao/app/Http/Controllers/ProdutoController.php
@@ -6,6 +6,7 @@ use App\Models\Item;
 use App\Models\Produto;
 
 use App\Http\Controllers\Controller;
+use App\Models\Fornecedor;
 use App\Models\ProdutoDetalhe;
 use App\Models\Unidade;
 use Illuminate\Http\Request;
@@ -46,9 +47,10 @@ class ProdutoController extends Controller
      */
     public function create()
     {
+        $fornecedores= Fornecedor::all();
         $unidades = Unidade::all();
 
-        return view("app.produto.create", ["unidades" => $unidades]);
+        return view("app.produto.create", ["unidades" => $unidades,"fornecedores"=>$fornecedores]);
     }
 
     /**
@@ -60,7 +62,8 @@ class ProdutoController extends Controller
             'nome' => 'required|min:3|max:40',
             'descricao' => 'required|min:3|max:2000',
             'peso' => 'required|integer',
-            'unidade_id' => 'exists:unidades,id'
+            'unidade_id' => 'exists:unidades,id',
+            'fornecedor_id' => 'exists:fornecedores,id'
         ];
         $feedback = [
             'required'=>'O campo :attribute deve ser preenchido',
@@ -69,11 +72,12 @@ class ProdutoController extends Controller
             'descricao.min'=>'O campo descricao deve ter no mínimo 3 caracteres',
             'descricao.max'=>'O campo descricao deve ter no máximo 2000 caracterers',
             'peso.integer'=>'O campo peso deve ser um número inteiro',
-            'unidade_id.exists'=>'A unidade de medida informada não existe'
+            'unidade_id.exists'=>'A unidade de medida informada não existe',
+            'fornecedor_id.exists' => 'O fornecedor informado não existe'
         ];
         $request->validate($regras,$feedback);
 
-        Produto::create($request->all());
+        Item::create($request->all());
 
         return redirect()->route("produto.index");
     }
@@ -93,15 +97,35 @@ class ProdutoController extends Controller
     public function edit(Produto $produto)
     {
         $unidades=Unidade::all();
-        return view("app.produto.edit", ["produto" => $produto,'unidades'=>$unidades]);
+        $fornecedores= Fornecedor::all();
+        return view("app.produto.edit", ["produto" => $produto,'unidades'=>$unidades,"fornecedores"=>$fornecedores]);
         //return view("app.produto.create", ["produto" => $produto,'unidades'=>$unidades]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Produto $produto)
+    public function update(Request $request, Item $produto)
     {
+        $regras = [
+            'nome' => 'required|min:3|max:40',
+            'descricao' => 'required|min:3|max:2000',
+            'peso' => 'required|integer',
+            'unidade_id' => 'exists:unidades,id',
+            'fornecedor_id' => 'exists:fornecedores,id'
+        ];
+        $feedback = [
+            'required'=>'O campo :attribute deve ser preenchido',
+            'nome.min'=>'O campo nome deve ter no mínimo 3 caracteres',
+            'nome.max'=>'O campo nome deve ter no máximo 40 caracterers',
+            'descricao.min'=>'O campo descricao deve ter no mínimo 3 caracteres',
+            'descricao.max'=>'O campo descricao deve ter no máximo 2000 caracterers',
+            'peso.integer'=>'O campo peso deve ser um número inteiro',
+            'unidade_id.exists'=>'A unidade de medida informada não existe',
+            'fornecedor_id.exists' => 'O fornecedor informado não existe'
+        ];
+        $request->validate($regras,$feedback);
+
         print_r($request->all());//Payload
         echo "<br><br><br>";
         print_r($produto->getAttributes()); //Instancia do objeto antes do payload
--- code/app_super_gestao/resources/views/app/produto/_components/form_create_edit.blade.php
+++ code/app_super_gestao/resources/views/app/produto/_components/form_create_edit.blade.php
@@ -8,6 +8,13 @@
     @csrf
 @endif
 
+     <select name="fornecedor_id">
+        <option>-- Selecione um Fornecedor --</option>
+        @foreach ($fornecedores as $fornecedor )
+            <option value="{{$fornecedor->id}}" {{($produto->fornecedor_id ?? old("fornecedor_id"))==$fornecedor->id?'selected':''}}>{{$fornecedor->nome}}</option>
+        @endforeach
+    </select>
+    {{$errors->has("fornecedor_id")?$errors->first('fornecedor_id'):''}}
     <input type="text" value="{{$produto->nome ?? old("nome")}}" name="nome" placeholder="Nome" class="borda-preta">
     {{$errors->has("nome")?$errors->first('nome'):''}}
     <input type="text" value="{{$produto->descricao ?? old("descricao")}}" name="descricao" placeholder="Descricao" class="borda-preta">
