cb23ae763f7b1d117e24db254dab0f5812eb8c03
Andy <andre.andre.jazz@gmail.com>
2024-08-16 13:53:25 -0300

    N para N pedido produto

--- code/app_super_gestao/routes/web.php
+++ code/app_super_gestao/routes/web.php
@@ -53,8 +53,9 @@ Route::middleware('autenticacao:padrao,visitante')->prefix('/app')->group(functi
     
     Route::resource('cliente',ClienteController::class);
     Route::resource('pedido',PedidoController::class);
-    Route::resource('pedido-produto',PedidoProdutoController::class);
-
+    //Route::resource('pedido-produto',PedidoProdutoController::class);
+    Route::get('pedido-produto/create/{pedido}',[PedidoProdutoController::class,'create'])->name('pedido-produto.create');
+    Route::post('pedido-produto/create/{pedido}',[PedidoProdutoController::class,'store'])->name('pedido-produto.store');
 });
 //Redirecionamento de rota
 // Route::get('/rota1', function () {
