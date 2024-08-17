b496663dc16e2e49bcb4df19725c1942d36eca3a
Andy <andre.andre.jazz@gmail.com>
2024-08-17 10:33:32 -0300

    Belongs to many

--- code/app_super_gestao/app/Models/Item.php
+++ code/app_super_gestao/app/Models/Item.php
@@ -21,7 +21,9 @@ class Item extends Model
     //use App\Models\Produto;
     //Produto::create(["nome"=>"Geladeira","descricao"=>"Geladeira/Refrigerador frost free 350 listros","peso"=>60,"unidade_id"=>1]);
     //Produto::create(["nome"=>"Smart TV","descricao"=>'Smart TV Led 43"',"peso"=>8,"unidade_id"=>1]);
-
+    public function pedidos(){
+        return $this->belongsToMany("App\Models\Pedido","pedidos_produtos","produto_id","pedido_id");
+    }
    
 
     use HasFactory;
