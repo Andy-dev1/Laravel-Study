1015c3210d375d164a21530e483d5b68c74d5475
Andy <andre.andre.jazz@gmail.com>
2024-09-06 21:07:24 -0300

    tarefa update

--- code/app_controle_tarefas/app/Http/Controllers/TarefaController.php
+++ code/app_controle_tarefas/app/Http/Controllers/TarefaController.php
@@ -84,7 +84,7 @@ class TarefaController extends Controller
      */
     public function edit(Tarefa $tarefa)
     {
-        //
+        return view("tarefa.edit",["tarefa"=>$tarefa]);
     }
 
     /**
@@ -92,7 +92,8 @@ class TarefaController extends Controller
      */
     public function update(Request $request, Tarefa $tarefa)
     {
-        //
+        $tarefa->update($request->all());
+        return redirect()->route("tarefa.show",["tarefa"=>$tarefa->id]);
     }
 
     /**
