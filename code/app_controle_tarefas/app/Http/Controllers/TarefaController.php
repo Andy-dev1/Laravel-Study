6e58d295f2aed23259fc179e9d3a5a5f226df78f
Andy <andre.andre.jazz@gmail.com>
2024-09-09 20:01:26 -0300

    Tarefas edit, delete and show

--- code/app_controle_tarefas/app/Http/Controllers/TarefaController.php
+++ code/app_controle_tarefas/app/Http/Controllers/TarefaController.php
@@ -84,7 +84,13 @@ class TarefaController extends Controller
      */
     public function edit(Tarefa $tarefa)
     {
-        return view("tarefa.edit",["tarefa"=>$tarefa]);
+        $user_id=Auth::user()->id;
+        if($tarefa->user_id==$user_id){
+            return view("tarefa.edit",["tarefa"=>$tarefa]);
+        }
+        
+        return view('acesso-negado');
+        
     }
 
     /**
@@ -92,8 +98,17 @@ class TarefaController extends Controller
      */
     public function update(Request $request, Tarefa $tarefa)
     {
+
+
+        if(!$tarefa->user_id==Auth::user()->id){
+            return view('acesso-negado');
+        }
+
         $tarefa->update($request->all());
         return redirect()->route("tarefa.show",["tarefa"=>$tarefa->id]);
+        
+
+       
     }
 
     /**
@@ -101,6 +116,10 @@ class TarefaController extends Controller
      */
     public function destroy(Tarefa $tarefa)
     {
-        //
+        if(!$tarefa->user_id==Auth::user()->id){
+            return view('acesso-negado');
+        }
+        $tarefa->delete();
+        return redirect()->route("tarefa.index");
     }
 }
