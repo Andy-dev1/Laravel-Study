fce230521bb0a152c2c28cd1dbae058f0f718620
Andy <andre.andre.jazz@gmail.com>
2024-09-10 14:08:07 -0300

    Excel

--- code/app_controle_tarefas/routes/web.php
+++ code/app_controle_tarefas/routes/web.php
@@ -5,7 +5,8 @@ use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return view('bem-vindo');
 });
 
 Auth::routes(['verify' => true]);
