3176f887d70758006888aaae4e1ce57af3179724
Andy <andre.andre.jazz@gmail.com>
2024-10-01 09:48:27 -0300

    refresh logout ttl

--- code/app_locadora_carros/routes/api.php
+++ code/app_locadora_carros/routes/api.php
@@ -15,16 +15,21 @@ Route::get('/user', function (Request $request) {
 
 // Route::resource('cliente',ClienteController::class);
 Route::prefix('v1')->middleware('jwt.auth')->group(function(){
+    Route::post('logout',[AuthController::class,'logout']);
+    Route::post('refresh',[AuthController::class,'refresh']);
+    Route::post('me',[AuthController::class,'me']);
+    
     Route::apiResource('cliente',ClienteController::class);
     Route::apiResource('carro',CarroController::class);
     Route::apiResource('locacao',LocacaoController::class);
     Route::apiResource('marca',MarcaController::class);
     Route::apiResource('modelo',ModeloController::class);
+    
 });
 
 
 
 Route::post('login',[AuthController::class,'login']);
-Route::post('logout',[AuthController::class,'logout']);
-Route::post('refresh',[AuthController::class,'refresh']);
-Route::post('me',[AuthController::class,'me']);
+
+
+
